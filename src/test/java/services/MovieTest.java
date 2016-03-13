750631d0a977e7ea88182e91773b33b2d89447bb
Hamza.HEDHLY <hamza.hedhly@esprit.tn>
2016-03-13 22:49:40 +0100

    movie, movie theaters n 3d movie theater services
    + delegate
    + test

---
+++ src/test/java/services/MovieTest.java
@@ -0,0 +1,51 @@
+package services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.tn.cinema.delegate.MovieServiceDelegate;
+import com.tn.cinema.entities.Movie;
+
+public class MovieTest {
+
+	@Test
+	@Ignore
+	public void addMovie() {
+		Movie m=new Movie();
+		m.setTitle("spiderman");
+		m.setLength(120L);
+		m.setType("adventure");
+		assertEquals(true,MovieServiceDelegate.addMovie(m));
+	}
+	
+	@Test
+	@Ignore
+	public void findMovieByID() {
+		assertEquals("LOTR", MovieServiceDelegate.findMovieByID(1).getTitle());
+	}
+	
+	@Test
+	@Ignore
+	public void updateMovie() {
+		Movie m=MovieServiceDelegate.findMovieByID(1);
+		m.setLength(228L);
+		assertEquals(true, MovieServiceDelegate.updateMovie(m));
+	}
+
+	@Test
+	@Ignore
+	public void findAllMovies() {
+		assertEquals(2, MovieServiceDelegate.findAllMovies().size());
+	}
+
+	@Test
+	@Ignore
+	public void deleteMovie() {
+		Movie m=MovieServiceDelegate.findMovieByID(2);
+		assertEquals(true, MovieServiceDelegate.deleteMovie(m));
+	}
+
+	
+}
---
+++ src/test/java/services/MovieTheaterTest.java
@@ -0,0 +1,61 @@
+package services;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.tn.cinema.delegate.ManagerServiceDelegate;
+import com.tn.cinema.delegate.MovieTheaterServiceDelegate;
+import com.tn.cinema.entities.Manager;
+import com.tn.cinema.entities.MovieTheater;
+
+public class MovieTheaterTest {
+
+	@Test
+	@Ignore
+	public void addMovieTheater() {
+		MovieTheater mt = new MovieTheater();
+		Manager m = ManagerServiceDelegate.findManagerByID(3);
+		mt.setManager(m);
+		mt.setAdresse("XXX, xx");
+		mt.setName("CCC");
+		mt.setCapacity(10L);
+		assertEquals(true, MovieTheaterServiceDelegate.addMovieTheater(mt));
+	}
+
+	@Test
+	@Ignore
+	public void findMovieTheaterByID() {
+		assertEquals(Long.valueOf(300), MovieTheaterServiceDelegate.findMovieTheaterByID(1).getCapacity());
+	}
+
+	@Test
+	@Ignore
+	public void updateMovieTheater() {
+		MovieTheater mt = MovieTheaterServiceDelegate.findMovieTheaterByID(1);
+		mt.setCapacity(325L);
+		assertEquals(true, MovieTheaterServiceDelegate.updateMovieTheater(mt));
+	}
+
+	@Test
+	@Ignore
+	public void getAllMovieTheaters() {
+		assertEquals(2, MovieTheaterServiceDelegate.getAllMovieTheaters().size());
+	}
+
+	@Test
+	@Ignore
+	public void getMovieTheatersByManager() {
+		Manager m = ManagerServiceDelegate.findManagerByID(3);
+		assertEquals(2, MovieTheaterServiceDelegate.getMovieTheatersByManager(m).size());
+		// assertEquals(Long.valueOf(400),MovieTheaterServiceDelegate.getMovieTheatersByManager(m).get(0).getCapacity());
+	}
+	
+	@Test
+	@Ignore
+	public void deleteMovieTheater() {
+		MovieTheater mt = MovieTheaterServiceDelegate.findMovieTheaterByID(3);
+		assertEquals(true,MovieTheaterServiceDelegate.deleteMovieTheater(mt));
+	}
+}
